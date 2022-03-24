f946d586331132efd34580941e26c6c23ffb0c6c
federix <gfederix@gmail.com>
2022-03-25 00:16:41 +0700

    ex3

--- src/com/company/CardB.java
+++ src/com/company/CardB.java
@@ -3,14 +3,62 @@ package com.company;
 public class CardB {
     private int rank;
     private int suit;
-    CardB( int rank, int suit ){
+    CardB(  int suit, int rank ){
         this.rank = rank;
         this.suit = suit;
     }
-    int GetRank(){
-        return rank;
+    public String getRank(){
+        switch ( rank + 1 ){
+            case (11): return "Jack";
+            case (12): return "Queen";
+            case (13): return "King";
+            case (14): return "Ace";
+            case (15): return "Joker";
+            default: return String.valueOf( rank );
+
+        }
     }
-    int GetSuit(){
-        return suit;
+    public String getSuit(){
+        switch ( suit ) {
+            case 0: return "Clubs";
+            case 1: return "Diamonds";
+            case 2: return "Spades";
+            case 3: return "Hearts";
+            default: return String.valueOf( suit );
+        }
     }
+    public String toString(){
+        return getSuit() + " of " + getRank();
+    }
+
+    public boolean isValid(){
+        return 0 <= suit && suit  <= 3 && 0 <=  rank && rank <= 15;
+    }
+    public boolean grater( CardB other ){
+        return other.suit == suit && other.rank < rank;
+    }
+    public int compare( CardB other ){
+        int suit_cmp = suit - other.suit;
+        if ( suit_cmp == 0 ) {
+            return  rank - other.rank;
+        }
+        return suit_cmp;
+    }
+    @Override
+    public boolean equals( Object obj ){
+        if ( this == obj )
+            return true;
+        if ( obj == null || getClass() != obj.getClass())
+            return false;
+        CardB other = (CardB) obj;
+        if ( other.rank == rank && other.suit == suit  )
+            return true;
+        return false;
+    }
+    @Override
+    public int  hashCode(){
+        final int prime = 31;
+        return prime * rank + suit;
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 import com.company.CardC;
+import com.company.CardB;
 
 public class Main {
 
@@ -8,5 +9,18 @@ public class Main {
         for (var card: cards ) {
             System.out.println(card);
         }
+        CardB [] cards3  = { new CardB(1, 4), new CardB(3, 13 )};
+        for (var card: cards3 ) {
+            System.out.println(card);
+        }
+        System.out.println(new CardB(1 , 3).compare( new CardB( 1, 3)));
+        System.out.println(new CardB(1 , 3).compare( new CardB( 1, 4)));
+        System.out.println(new CardB(2 , 3).compare( new CardB( 1, 4)));
+        System.out.println(new CardB(1 , 3).hashCode() ==  new CardB( 1, 3).hashCode());
+        System.out.println(new CardB(1 , 3).hashCode() !=  new CardB( 0, 3).hashCode());
+        System.out.println(new CardB(1 , 3).equals(new CardB(1, 3)));
+        System.out.println(! new CardB(1 , 3).equals(new CardB(1, 1)));
+
+
     }
 }
